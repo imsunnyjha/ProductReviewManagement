69f28084ecd171ef05fc8835ae46e5e5275cd517
Sunny Jha <sunnyjha11897@gmail.com>
2020-11-05 16:26:11 +0530

    [SUNNY]Add.Welcome to product review management

---
+++ ProductReviewManagementLinq/ProductReview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagementLinq
+{
+    public class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+
+    }
+}
